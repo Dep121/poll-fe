659ef89e96cf95178dad32dbf20d06feec2048e2
= <=>
2021-03-03 09:40:09 +0530

    api integrated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Header.scss';
 import Button from "../Button/Button";
 
-function Header(){
-    return(
+function Header() {
+
+    const [userName, setUserName] = useState(null);
+    const [email, setEmail] = useState(null);
+
+    async function getUserInfo() {
+        let res = await fetch('http://localhost:3001/users/1');
+        let result = await res.json();
+        const {name, email} = result[0];
+
+        setUserName(name);
+        setEmail(email);
+    }
+
+    useEffect(()=>{
+        getUserInfo();
+    },[])
+
+    return (
         <header className={'header'} >
             <a className={'left'} href={'http://localhost:3000/'} title={'Rapid Poll'}></a>
             <div className={'mid'}>
                 create Poll
             </div>
             <div className={'right'}>
-                <Button onClick={()=>{}}>login</Button>
-                <Button filled onClick={()=>{}}>signUp</Button>
+                {
+                    userName
+                        ? <div className={'user-details'}>
+                            <div>Welcome {userName} </div>
+                            <div>email: {email} </div>
+                        </div>
+                        : <>
+                            <Button onClick={() => { }}>login</Button>
+                            <Button filled onClick={() => { }}>signUp</Button>
+                        </>
+                }
             </div>
         </header>
     )
